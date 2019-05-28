8515331e9b25d02b53de9953bb83b5ce793fab67
aharonha <aharon@om2.com>
2019-05-28 17:46:49 +0300

    test-proof

---
+++ scalecube-fixtures-tck-flow/src/test/java/io/scalecube/test/fixtures/FailingFixtureConstructor.java
@@ -0,0 +1,33 @@
+package io.scalecube.test.fixtures;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.opentest4j.TestAbortedException;
+
+public class FailingFixtureConstructor implements Fixture {
+
+  boolean setupStarted = false;
+
+  public FailingFixtureConstructor() {
+    throw new IllegalArgumentException();
+  }
+
+  @Override
+  public void setUp() throws TestAbortedException {
+    setupStarted = true;
+    throw new TestAbortedException("Fixture Setup Failed");
+  }
+
+  @Override
+  public <T> T proxyFor(Class<? extends T> clasz) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public void tearDown() {
+    System.out.println(" terminating " + this.getClass().getSimpleName());
+    assertFalse(setupStarted);
+  }
+}
---
+++ scalecube-fixtures-tck-flow/src/test/java/io/scalecube/test/fixtures/FailingFixtureInSetup.java
@@ -0,0 +1,28 @@
+package io.scalecube.test.fixtures;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.opentest4j.TestAbortedException;
+
+public class FailingFixtureInSetup implements Fixture {
+
+  boolean setupStarted = false;
+
+  @Override
+  public void setUp() throws TestAbortedException {
+    setupStarted = true;
+    throw new TestAbortedException("Fixture Setup Failed");
+  }
+
+  @Override
+  public <T> T proxyFor(Class<? extends T> clasz) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public void tearDown() {
+    System.out.println(" terminating " + this.getClass().getSimpleName());
+    assertTrue(setupStarted);
+  }
+}
