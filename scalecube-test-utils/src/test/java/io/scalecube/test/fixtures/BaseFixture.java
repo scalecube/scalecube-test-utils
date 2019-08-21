3288ad5c2651cf20769c31f7b5706afbe27d3279
aharonha <aharon@om2.com>
2019-08-21 17:20:05 +0300

    fix tests

--- scalecube-test-utils/src/test/java/io/scalecube/test/fixtures/BaseFixture.java
+++ scalecube-test-utils/src/test/java/io/scalecube/test/fixtures/BaseFixture.java
@@ -42,18 +42,21 @@ public class BaseFixture implements Fixture {
   protected BaseFixture(InterfaceToSomeService withSomeInterface) {
     constructorWithInterfaceWasCalled = true;
     this.properties = System.getProperties();
+    withSomeInterface.method();
   }
 
   protected BaseFixture(Properties p, InterfaceToSomeService withSomeInterface) {
     constructorWithInterfaceWasCalled = true;
     constructorWithPropertiesWasCalled = true;
     this.properties = p;
+    withSomeInterface.method();
   }
 
   protected BaseFixture(InterfaceToSomeService withSomeInterface, Properties p) {
     constructorWithInterfaceWasCalled = true;
     constructorWithPropertiesWasCalled = true;
     this.properties = p;
+    withSomeInterface.method();
   }
 
   @Override
--- scalecube-test-utils/src/test/java/io/scalecube/test/fixtures/CallingInterfaceConstructor.java
+++ scalecube-test-utils/src/test/java/io/scalecube/test/fixtures/CallingInterfaceConstructor.java
@@ -1,6 +1,30 @@
 package io.scalecube.test.fixtures;
 
-public class CallingInterfaceConstructor extends BaseFixture {
+import org.opentest4j.TestAbortedException;
+
+public class CallingInterfaceConstructor implements Fixture {
+  private final InvocationSpyFixture spy =
+      new InvocationSpyFixture() {
+
+        @Override
+        public boolean constructorWithPropertiesWasCalled() {
+          return constructorWithPropertiesWasCalled;
+        }
+
+        @Override
+        public boolean constructorWithInterfaceWasCalled() {
+          return constructorWithInterfaceWasCalled;
+        }
+
+        @Override
+        public boolean constructorWasCalled() {
+          return constructorWasCalled;
+        }
+      };
+
+  private boolean constructorWasCalled = false;
+  private boolean constructorWithPropertiesWasCalled = false;
+  private boolean constructorWithInterfaceWasCalled = false;
   private static InterfaceToSomeService service =
       new InterfaceToSomeService() {
         @Override
@@ -10,6 +34,32 @@ public class CallingInterfaceConstructor extends BaseFixture {
       };
 
   public CallingInterfaceConstructor() {
-    super(service);
+    constructorWithInterfaceWasCalled = true;
+  }
+
+  @Override
+  public <T> T proxyFor(Class<? extends T> clasz) {
+    if (clasz.isAssignableFrom(InterfaceToSomeService.class)) {
+      return clasz.cast(service);
+    }
+    if (clasz.isAssignableFrom(InvocationSpyFixture.class)) {
+      return clasz.cast(this.spy);
+    }
+    if (clasz.isAssignableFrom(Class.class)) {
+      return clasz.cast(this.getClass());
+    }
+    return null;
+  }
+
+  @Override
+  public void setUp() throws TestAbortedException {
+    // TODO Auto-generated method stub
+
+  }
+
+  @Override
+  public void tearDown() {
+    // TODO Auto-generated method stub
+
   }
 }
